3f7bad3c44ccc1997ab2008b4dc2170a7c8870d4
Impuls3iv <sammy.unterman@gmail.com>
2016-07-07 21:16:26 -0400

    button click loading list working. need to make button and on load call same function

--- election.js
+++ election.js
@@ -1,14 +1,21 @@
 $(function(){
 
   // Imagination!
-  $.ajax({
-    url:"https://bb-election-api.herokuapp.com/",
-    method: 'get',
-    data: { },
-    dataType: 'json'
-  }).done(function(responseData){
-    $('<li>').appendTo('#list').html(responseData.candidates[0].name);
-
-  }); //end of request
+  $('#button').on('click', function(){
+    $('#list').html('');
+    $.ajax({
+      url:"https://bb-election-api.herokuapp.com/",
+      method: 'get',
+      data: { },
+      dataType: 'json'
+    }).done(function(responseData){
+      for (i = 0; i < responseData.candidates.length; i++) { //go through all candidates
+        $('<li>').appendTo('#list')
+                 .html(responseData.candidates[i].name);
+        $('<li>').appendTo('#list')
+                 .html(responseData.candidates[i].votes);
+      };//end for loop
+    }); //end of request
+  })//end of click
 
 });
